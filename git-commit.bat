fa740b96a4a69ddb5b090627c337521effcae7bf
Celia <cemolina.28@gmail.com>
2024-10-17 22:19:56 +0200

    "Actualizado bat de git commit"
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   git-commit.bat
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   git-commit.bat
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    	commit_message.txt

--- git-commit.bat
+++ git-commit.bat
@@ -1,6 +1,15 @@
 @echo off
 set MENSAJE=%1
 
+REM Crear un archivo temporal para el mensaje del commit
+echo %MENSAJE% > commit_message.txt
+echo. >> commit_message.txt
+git status >> commit_message.txt
+
+REM Agregar los cambios y realizar el commit usando el archivo temporal
 git add .
-git commit -am "%MENSAJE%"
+git commit -F commit_message.txt
 git push
+
+REM Eliminar el archivo temporal
+del commit_message.txt
